ecbc6f985f7ca0a787b5a3f149fd62b770775a36
Hrushikesh Bhoir <rushikeshbhoir72@gmail.com>
2026-07-03 19:21:28 +0530

    remove unwanted  frontend resources

--- client old/src/hooks/useNotifications.js
+++
@@ -1,6 +0,0 @@
-import { useContext } from 'react';
-import { NotificationContext } from '../context/NotificationContext.jsx';
-
-const useNotifications = () => useContext(NotificationContext);
-
-export default useNotifications;
